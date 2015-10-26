68a75ce8f9c2b3287d274310ba61beaa1e76233b
Kasey Delk <delkk@student.swosu.edu>
2015-10-26 09:51:05 -0500

    trimmed up code

--- src/Fun_With_Functions_Project.java
+++ src/Fun_With_Functions_Project.java
@@ -1,19 +1,5 @@
-/**
- * 
- */
-
-/**
- * @author LAB
- *
- */
 public class Fun_With_Functions_Project {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
 }
