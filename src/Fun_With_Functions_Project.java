97ea1240a42393bfa84935d9c9e5b862e15b128a
Kasey Delk <delkk@student.swosu.edu>
2015-10-26 09:33:23 -0500

    First commit

---
+++ src/Fun_With_Functions_Project.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAB
+ *
+ */
+public class Fun_With_Functions_Project {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
